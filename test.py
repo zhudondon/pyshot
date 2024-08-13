3976d034677bc30e7d130c4a2cf9dd754c8ba057
zhuxiaodong <zhuxiaodong@eenet.com>
2024-08-13 18:13:02 +0800

    初始化

---
+++ test.py
@@ -0,0 +1,9 @@
+
+
+
+abc = "getExamRecordDetail('703cb6853d8a423896ec305e14084845')"
+
+abc = abc.replace("getExamRecordDetail(","")
+abc = abc.replace(")","")
+abc = abc.replace("'","")
+print(abc)
